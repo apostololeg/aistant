69a6786c5c4a8d40ffa1cc241a30e3d948b2fb58
Oleg Apostol <truerenton@gmail.com>
2023-09-24 19:28:00 +0100

    fix

--- src/stores/nodes.ts
+++ src/stores/nodes.ts
@@ -2,22 +2,24 @@ import { createStore } from 'justorm/react';
 import { LS } from '@homecode/ui';
 import ws from 'stores/ws';
 
-const localNodes = LS.get('nodes') || [];
-
 export default createStore('nodes', {
-  nodes: new Set(localNodes),
+  // @ts-ignore
+  nodes: new Set(LS.get('nodes')),
+
   async loadNodes() {
     console.log('### loadNodes');
 
-    ws.socket.emit('nodes');
+    ws.socket.emit('nodes', {});
     ws.socket.on('nodes', nodes => {
-      this.nodes = new Set(nodes);
-      LS.set('nodes', Array.from(this.nodes));
+      this.nodes = new Set(JSON.parse(nodes));
+      LS.set('nodes', [...this.nodes.originalObject]);
     });
   },
+
   addNode(node) {
     this.nodes.add(node);
   },
+
   removeNode(node) {
     this.nodes.remove(node);
   },
